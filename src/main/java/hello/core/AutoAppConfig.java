605335a79863e78463ed5dd107326c7fd9a09582
baek.gang.hyeon <weiss1129@gmail.com>
2024-01-19 21:42:19 +0900

    중복 등록과 충돌

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package hello.core;
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -14,6 +17,12 @@ import org.springframework.context.annotation.FilterType;
 // basePackageClasses는 통상 지정하지 않고 프로젝트 패키지 구조 최상단에...
 public class AutoAppConfig {
 
-
+    // 이 경우 수동 빈 등록이 우선권을 가진다.
+    // 수동 빈이 자동 빈을 오버라이딩
+    // 에러가 나도록 스프링부트에서 최근 바뀜...
+    @Bean(name = "memoryMemberRepository")
+    MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
 
 }
