81abde7dc392adf4a08dea76c95a67745363360c
baek.gang.hyeon <weiss1129@gmail.com>
2024-01-21 23:59:57 +0900

    옵션 처리

--- src/test/java/hello/core/autowired/AutowiredTest.java
+++ src/test/java/hello/core/autowired/AutowiredTest.java
@@ -1,7 +1,12 @@
 package hello.core.autowired;
 
+import hello.core.member.Member;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.lang.Nullable;
+
+import java.util.Optional;
 
 public class AutowiredTest {
 
@@ -12,6 +17,21 @@ public class AutowiredTest {
     }
 
     static class TestBean {
+
+        @Autowired(required = false)
+        public void setNoBean1(Member noBean1) {
+            System.out.println("noBean1 = " + noBean1);
+        }
+
+        @Autowired
+        public void setNoBean2(@Nullable Member noBean1) {
+            System.out.println("noBean2 = " + noBean1);
+        }
+
+        @Autowired
+        public void setNoBean3(Optional<Member> noBean3) {
+            System.out.println("noBean3 = " + noBean3);
+        }
 
     }
 
