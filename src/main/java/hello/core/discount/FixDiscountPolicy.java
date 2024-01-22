aa532c3e3dd98d4eced2c5478305e0419484f847
baek.gang.hyeon <weiss1129@gmail.com>
2024-01-22 23:44:57 +0900

    @Autowired 필드 명, @Qualifier, @Primary

--- src/main/java/hello/core/discount/FixDiscountPolicy.java
+++ src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -4,7 +4,7 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import org.springframework.stereotype.Component;
 
-//@Component
+@Component
 public class FixDiscountPolicy implements DiscountPolicy {
 
     private int discoutFixAmount = 1000; //1000원 정액 할인
