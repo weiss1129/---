ffecffad18bf61e0035bd3b9e2facdbbddb9319f
baek.gang.hyeon <weiss1129@gmail.com>
2024-01-29 00:35:30 +0900

    스코프와 Provider

--- src/main/java/hello/core/common/MyLogger.java
+++ src/main/java/hello/core/common/MyLogger.java
@@ -13,23 +13,21 @@ public class MyLogger {
 
     private String uuid;
     private String requestURL;
-
     public void setRequestURL(String requestURL) {
         this.requestURL = requestURL;
     }
-
     public void log(String message) {
-        System.out.println("[" + uuid + "]" + " [" + requestURL + "] " + message);
+        System.out.println("[" + uuid + "]" + "[" + requestURL + "] " +
+                message);
     }
-
     @PostConstruct
     public void init() {
         uuid = UUID.randomUUID().toString();
-        System.out.println("[" + uuid + "] request scope bean create : " + this);
+        System.out.println("[" + uuid + "] request scope bean create:" + this);
     }
-
     @PreDestroy
     public void close() {
-        System.out.println("[" + uuid + "] request scope bean close : " + this);
+        System.out.println("[" + uuid + "] request scope bean close:" + this);
     }
+
 }
